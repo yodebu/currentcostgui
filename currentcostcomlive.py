0301fe4ab636700c38c5efa3474dc10f7f739e61
dale.lane <dale.lane@gmail.com>
2009-06-03 20:58:36 +0000

    Adding support for multi-phase installs

--- currentcostcomlive.py
+++ currentcostcomlive.py
@@ -57,29 +57,23 @@ class CurrentCostSerialLiveConnection():
         while self.toCancel == False:
             try:
                 line = self.ser.readUpdate()
-                
-                idx = line.find('<ch1><watts>')
-                if idx > 0:
-                    idx += 12
-                    endidx = line.find('</watts></ch1>', idx)
-                    if endidx > 0:
-                        substr = line[idx : endidx]
-                        try:
-                            ccreading = float(float(substr) / 1000)
-                            guihandle.updateGraph(ccreading)
-                        except:            
-                            # Exiting on a single garbled string from the 
-                            #  serial port is a bit extreme - it isn't unusual 
-                            #  to get a partial string, particularly if we've 
-                            #  been reading from the serial port for a few 
-                            #  hours
-                            # So we count the number of times we hit an error
-                            #  and only exit after we see ten of them
-                            if self.numberOfErrors < 10:
-                                self.numberOfErrors += 1
-                            else:
-                                guihandle.exitOnError('Unable to parse reading from meter: ' + str(substr))
-                                return
+
+                try:
+                    ccreading = self.parseLiveXML(line)                
+                    guihandle.updateGraph(ccreading)
+                except:            
+                    # Exiting on a single garbled string from the 
+                    #  serial port is a bit extreme - it isn't unusual 
+                    #  to get a partial string, particularly if we've 
+                    #  been reading from the serial port for a few 
+                    #  hours
+                    # So we count the number of times we hit an error
+                    #  and only exit after we see ten of them
+                    if self.numberOfErrors < 10:
+                        self.numberOfErrors += 1
+                    else:
+                        guihandle.exitOnError('Unable to parse reading from meter: ' + str(line))
+                        return
             except Exception, exception:
                 if self.toCancel == False:
                     guihandle.exitOnError('Error reading from COM port: ' + str(exception))
@@ -89,6 +83,62 @@ class CurrentCostSerialLiveConnection():
             self.ser.disconnect()
         except:
             self.guicallback.exitOnError('Error when closing COM port')
+
+
+    #
+    # Parse live XML
+    # 
+    # Read a line of XML, identify the three live channel watts values, and 
+    #  return the sum of them
+    # 
+    def parseLiveXML(self, line):
+
+        START_TAG_LENGTH = 12    # 12 is the length of the "<chX><watts>" string
+        END_TAG_LENGTH = 14    # 14 is the length of the "</watts></chX>" string
+
+        ccreading = 0
+
+        idx = line.find('<ch1><watts>')
+        if idx <= 0:
+            raise Exception('Unable to find an opening ch1 tag')
+        idx += START_TAG_LENGTH
+
+        endidx = line.find('</watts></ch1>', idx)
+        if endidx <= 0:
+            raise Exception('Unable to find a closing ch1 tag')
+
+        substr = line[idx : endidx]
+
+        ccreading += float(float(substr) / 1000)
+
+        idx = line.find('<ch2><watts>', endidx)
+        if idx <= 0:
+            raise Exception('Unable to find an opening ch2 tag')
+        idx += START_TAG_LENGTH
+
+        endidx = line.find('</watts></ch2>', idx)
+        if endidx <= 0:
+            raise Exception('Unable to find a closing ch2 tag')
+
+        substr = line[idx : endidx]
+
+        ccreading += float(float(substr) / 1000)
+
+        idx = line.find('<ch3><watts>', endidx)
+        if idx <= 0:
+            raise Exception('Unable to find an opening ch3 tag')
+        idx += START_TAG_LENGTH
+
+        endidx = line.find('</watts></ch3>', idx)
+        if endidx <= 0:
+            raise Exception('Unable to find a closing ch3 tag')
+
+        substr = line[idx : endidx]
+
+        ccreading += float(float(substr) / 1000)
+
+        return ccreading
+
 
 
     #
