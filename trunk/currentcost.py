921bfa2cc3e917d66d3d92568c58356ffed83783
dale.lane@gmail.com <dale.lane@gmail.com@b4d4d52e-a485-11dd-b47b-0378ca0a891c>
2009-12-08 17:49:27 +0000

    timezone issues

--- trunk/currentcost.py
+++ trunk/currentcost.py
@@ -452,7 +452,7 @@ class MyFrame(wx.Frame):
         info.SetName('CurrentCost')
         info.Developers = ['Dale Lane']
         info.Description = "Draws interactive graphs using the data from a CurrentCost electricity meter"
-        info.Version = "0.9.29"
+        info.Version = "0.9.30"
         info.WebSite = ("http://code.google.com/p/currentcostgui/", "http://code.google.com/p/currentcostgui/")
         wx.AboutBox(info)
 
@@ -508,7 +508,7 @@ class MyFrame(wx.Frame):
                                        style=(wx.OK | wx.ICON_EXCLAMATION))
             result = confdlg.ShowModal()        
             confdlg.Destroy()
-        elif latestversion != "0.9.29":
+        elif latestversion != "0.9.30":
             confdlg = wx.MessageDialog(self,
                                        "A newer version of this application (" + latestversion + ") is available.\n\n"
                                        "Download now?",
